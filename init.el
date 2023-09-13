d9152fc0ebc8c7c819b576546a6e2d7c17e20643
Kong Jun <kongjun18@outlook.com>
2023-09-13 21:16:52 +0800

    Start the journal of Emacs!

---
+++ init.el
@@ -0,0 +1,19 @@
+(setq inhibit-startup-message t)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(menu-bar-mode -1)
+
+(global-display-line-numbers-mode 1)
+
+(setq package-archives '(("gnu"    . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                         ("nongnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
+                         ("melpa"  . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package which-key
+  :ensure t
+  :config (which-key-mode))
